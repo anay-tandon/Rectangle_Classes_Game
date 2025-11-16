22f2e69d6195773d1eeb3d9c77e964cd3569b0c2
Anay Tandon <uk.anay.tandon@gmail.com>
2025-11-16 18:11:31 +0000

    Enhance Rectangle class with grow and shrink methods
    Refactor Rectangle class to include grow and shrink methods. Update draw method to use width, height, and position attributes.

--- HW 3/rect.py
+++ HW 3/rect.py
@@ -10,17 +10,51 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 
 class Rectangle:
-    def __init__(screen, self, rect, color, width = 0):
-        self.rect = rect
+    def __init__(self, screen, pos, rw, rh, color, width = 0):
+        self.rw = rw
+        self.rh = rh
+        self.pos = pos
         self.color = color
         self.width = width
         self.screen = screen
 
     def draw(self):
-        pygame.draw.rect(self.screen, self.rect, self.color, self.width)
+        pygame.draw.rect(self.screen, self.color, (self.pos[0], self.pos[1], self.rw, self.rh), self.width)
 
-width = 2
-rect = (100, 100, 100, 150)
+    def grow(self):
+        self.rw += 10
+        self.rh += 10
+        pygame.draw.rect(self.screen, self.color, (self.pos[0], self.pos[1], self.rw, self.rh), self.width)
 
-pygame.draw.rect(screen, red, rect, width)
-pygame.display.update()
+    def shrink(self):
+        self.rw -= 10
+        self.rh -= 10
+        pygame.draw.rect(self.screen, self.color, (self.pos[0], self.pos[1], self.rw, self.rh), self.width)
+
+rect1 = Rectangle(screen, (200, 200), 100, 150, red, 0)
+rect2 = Rectangle(screen, (200, 200), 150, 200, green, 5)
+rect3 = Rectangle(screen, (200, 200), 200, 250, yellow, 10)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            rect1.draw()
+            rect2.draw()
+            rect3.draw()
+            pygame.display.update()
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            rect1.grow()
+            rect2.grow()
+            rect3.grow()
+            pygame.display.update()
+
+        elif event.type == pygame.KEYDOWN:
+            rect1.shrink()
+            rect2.shrink()
+            rect3.shrink()
+            pygame.display.update()
+
+
+
+            
